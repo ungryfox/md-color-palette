d95bfe5eb0a39cac1faf416264f988c5f29c80d4
Bruno Wego <brunowego@gmail.com>
2015-12-11 11:53:39 -0200

    chore(npm): add cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,17 @@ module.exports = function(grunt) {
         }]
       }
     },
+    cssmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'dist/styles',
+          src: ['*.css'],
+          dest: 'dist/styles',
+          ext: '.min.css'
+        }]
+      }
+    },
     browserSync: {
       options: {
         background: true
@@ -45,11 +56,13 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-browser-sync');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('dist', [
-    'sass:dist'
+    'sass:dist',
+    'cssmin:dist'
   ]);
 
   grunt.registerTask('server', [
